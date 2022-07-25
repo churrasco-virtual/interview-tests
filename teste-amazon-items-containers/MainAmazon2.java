709a62bb20e78de531ee3445a41bcce70ce613ca
aquila <aquila.antonio@hotmail.com>
2022-07-25 11:16:36 -0300

    update teste amazon2

--- teste-amazon-items-containers/MainAmazon2.java
+++ teste-amazon-items-containers/MainAmazon2.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.joining;
@@ -11,8 +15,8 @@ public class MainAmazon2 {
     public static final String LOCAL_PATH = ".\\teste-amazon-items-containers";
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(LOCAL_PATH + "\\entrada")));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(LOCAL_PATH +"\\saida"));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(LOCAL_PATH + "\\entrada2")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(LOCAL_PATH + "\\saida"));
 
         String s = bufferedReader.readLine();
 
@@ -56,6 +60,22 @@ public class MainAmazon2 {
     }
 
     public static List<Integer> numberOfItems(String s, List<Integer> startIndices, List<Integer> endIndices) {
-        return new ArrayList<>();
+        List<Integer> retorno = new ArrayList<>();
+
+        for (int i = 0; i < startIndices.size(); i++) {
+
+            int start = startIndices.get(i) - 1;
+            int end = endIndices.get(i);
+            String substring = s.substring(start, end);
+            Matcher matcher = Pattern.compile("\\|([^\\]]*)\\|").matcher(substring);
+            int count = 0;
+            while (matcher.find()) {
+                final int frequency = Collections.frequency(Arrays.asList(matcher.group().split("|")), "*");
+                count += frequency;
+            }
+            retorno.add(count);
+        }
+
+        return retorno;
     }
 }
